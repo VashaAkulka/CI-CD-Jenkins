152772265160619f330dbf949df0fdb894ed0ec7
vitya <myemail@mail.ru>
2024-05-16 11:37:30 +0300

    finish screenshot and deploy script

---
+++ docker_deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker stop spring_application || true
+docker rm spring_application || true
+docker build -t application .
+docker run -p 8081:8081 -d --name spring_application application
