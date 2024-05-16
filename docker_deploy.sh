ae4b5482c89dceaedcb998b06881cbc02659450b
vitya <myemail@mail.ru>
2024-05-16 12:10:24 +0300

    JenkinsLog file

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -2,5 +2,6 @@
 
 docker stop spring_application || true
 docker rm spring_application || true
+docker image rm application || true
 docker build -t application .
 docker run -p 8081:8081 -d --name spring_application application
